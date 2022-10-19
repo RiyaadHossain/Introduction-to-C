cc9944bc8c7ec63372d0689dcdae719862e7cef4
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-19 21:18:00 +0600

    basic of c

---
+++ problem1.c
@@ -0,0 +1,11 @@
+// To calculate area of Square
+# include <stdio.h>
+
+int main(){
+    int area;
+    printf("Enter area of the Square:");
+    scanf("%d", &area);
+    area = area * area;
+    printf("Square of the area is %d", area);
+    return 0;
+}
