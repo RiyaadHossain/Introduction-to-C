41047c28f9975c7d8f30075e1e555d37b14d6545
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-30 23:29:19 +0600

    ..

--- IO.c
+++ IO.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 
-int main(){
-    FILE *fpt;
-    fpt = fopen("test.txt", "r");
+int main()
+{
+    /*    FILE *fpt;
+       fpt = fopen("test.txt", "r");
 
-    int n;
-    fscanf(fpt, "%d", &n);
-    printf("Number is - %d \n", n);
-    fscanf(fpt, "%d", &n);
-    printf("Number is - %d \n", n);
-    fscanf(fpt, "%d", &n);
-    printf("Number is - %d \n", n);
-    fscanf(fpt, "%d", &n);
-    printf("Number is - %d \n", n);
-    fscanf(fpt, "%d", &n);
-    printf("Number is - %d \n", n);
+       int n;
+       fscanf(fpt, "%d", &n);
+       printf("Number is - %d \n", n);
+       fscanf(fpt, "%d", &n);
+       printf("Number is - %d \n", n);
+       fscanf(fpt, "%d", &n);
+       printf("Number is - %d \n", n);
+       fscanf(fpt, "%d", &n);
+       printf("Number is - %d \n", n);
+       fscanf(fpt, "%d", &n);
+       printf("Number is - %d \n", n); */
+
+    /* FILE *fptr;
+    fptr = fopen("string.txt", "r");
+    char greet[300];
+
+    fscanf(fptr, "%s", &greet);
+    printf("%s \t", greet);
+
+    fscanf(fptr, "%s", &greet);
+    printf("%s \t", greet);
+
+    fscanf(fptr, "%s", &greet);
+    printf("%s \t", greet); */
+
+    FILE *fptr;
+    fptr = fopen("stu.txt", "w");
+
+    char name[100];
+    int age;
+    int roll;
+
+    printf("Enter name: ");
+    scanf("%s", name);
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    printf("Enter roll: ");
+    scanf("%d", &roll);
+
+    fprintf(fptr, "Name - %s \n", name);
+    fprintf(fptr, "Age - %d \n", age);
+    fprintf(fptr, "Roll - %d \n", roll);
+
+    fclose(fptr);
 
     return 0;
 }
